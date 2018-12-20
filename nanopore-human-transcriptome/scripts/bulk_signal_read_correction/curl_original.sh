cf348a029004e5d85e6de627a3e705303d002e9c
Alex Payne <alexander.payne@nottingham.ac.uk>
2018-12-20 17:37:34 +0000

    Use zgrep

---
+++ nanopore-human-transcriptome/scripts/bulk_signal_read_correction/curl_original.sh
@@ -0,0 +1,13 @@
+# $1 is the input name
+
+zgrep -F -f "$1"/ends_filenames.txt Notts_all.txt > "$1"/ends/urls.txt
+zgrep -F -f "$1"/starts_filenames.txt Notts_all.txt > "$1"/starts/urls.txt
+
+mkdir -p "$1"/ends/original/fast5
+cd $_
+xargs -n 1 curl -O# < ../../urls.txt
+cd ../../../
+mkdir -p starts/original/fast5
+cd $_
+xargs -n 1 curl -O# < ../../urls.txt
+cd ../../../
